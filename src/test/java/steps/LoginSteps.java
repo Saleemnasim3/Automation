7049b3fc16b2ed4e596dcbcd746d502f1b501843
Saleem Sadat Mohammad Nasim <saleem.sadat@teladochealth.com>
2023-02-23 11:20:13 -0500

    first commit

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,39 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.SQLOutput;
+
+public class LoginSteps {
+
+
+WebDriver driver;
+    @Given("user is on amazon website")
+    public void user_is_on_amazon_website() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Saleem\\Desktop\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.navigate().to("http://amazon.com");
+    }
+
+    @When("user clicks on loing feature")
+    public void user_clicks_on_loing_feature() {
+        System.out.println("Click on login feature");
+    }
+
+    @Then("user should be authenticated")
+    public void user_should_be_authenticated() {
+        System.out.println("User is authenticated");
+    }
+
+
+
+
+
+}
+
+
---
+++ target/test-classes/Login.feature
@@ -0,0 +1,5 @@
+Feature: Login Feature
+  Scenario: User cannot login
+    Given user is on amazon website
+    When user clicks on loing feature
+    Then user should be authenticated
